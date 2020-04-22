6b9e56f12a7eaa24fc0df47914657580316a31ca
Cory Gillenkirk <cgillenkirk@gmail.com>
2020-04-22 16:54:54 -0400

    Update fix-boot.sh

--- fix-boot.sh
+++ fix-boot.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
-yum -y install psmisc net-tools
+# bash fix-boot.sh [rescue]
 
-# Yum Update
+efi="$(grep "efi" /etc/fstab | awk '{print $1}')"
+boot=$1
+
 yum clean all
 yum -y update
 yum -y reinstall kernel kernel-headers
 
-# SELinux
 /sbin/setenforce 0
 sed -i 's/SELINUX=enforcing/\ESELINUX=disabled/g' /etc/selinux/config
-echo 0 >/selinux/enforce
 
-# Fix Grub (Only OVH)
-efi="$(grep "efi" /etc/fstab | awk '{print $1}')"
-cd /
-grub2-mkconfig -o /boot/grub2/grub.cfg
-grub2-install $efi
+function fix-boot {
+	cd /
+	grub2-mkconfig -o /boot/grub2/grub.cfg
+	grub2-install $efi
+}
+
+# Fix Grub
+# Look into a solution to auto-detect rescue mode.
+if [ "${boot}" == "rescue" ]; then
+	sudo mount /dev/sdxy /mnt
+	sudo mount --bind /dev /mnt/dev
+	sudo mount --bind /proc /mnt/proc
+	sudo mount --bind /sys /mnt/sys
+	fix-grub
+	sudo chroot /mnt
+	sudo umount /mnt/dev
+	sudo umount /mnt/proc
+	sudo umount /mnt/sys
+	sudo umount /mnt
+else
+	fix-boot
+fi
